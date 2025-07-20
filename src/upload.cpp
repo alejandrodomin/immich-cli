ec36c6e0e9247f33410e7afa84c2733eaa253fea
A283le <alexdominguez169@gmail.com>
2025-07-20 15:10:54 -0400

    iteration over sub files working
    next steps are to check the immich-cli to see what they do during the
    crawling process

--- src/upload.cpp
+++ src/upload.cpp
@@ -1,7 +1,18 @@
 #include "../lib/upload.hpp"
 
+#include <filesystem>
 #include <iostream>
 
 using namespace std;
+namespace fs = std::filesystem;
 
-void upload() { cout << "Starting server upload." << endl; }
+void upload() {
+    fs::path curr_dir = fs::current_path();
+
+    cout << "All files under " << curr_dir << "\n";
+    for (const auto& file : fs::recursive_directory_iterator(curr_dir)) {
+        if (fs::file_type::regular == fs::status(file).type()) {
+            cout << "\t" << file << "\n";
+        }
+    }
+}
